99a4e2d67f3dcd3b5e7d10cb3b69190302f4986f
Tomáz <tomazcarballo@gmail.com>
2025-11-05 23:48:28 -0300

    Configuração do Vercel

--- metro.config.cjs
+++ metro.config.cjs
@@ -5,4 +5,9 @@ const config = getDefaultConfig(__dirname);
 // Configurações específicas para web
 config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 
+// Configurar resolução de arquivos WASM como assets (não como source)
+config.resolver.assetExts.push('wasm');
+config.resolver.sourceExts = config.resolver.sourceExts.filter(ext => ext !== 'wasm');
+
 module.exports = config;
+
